844925813880c38824237819efbca830327fb36f
László Velinszky <laszlo.velinszky@meltwater.com>
2015-04-21 23:46:18 +0200

    directive loads dynamically

--- client/app/sosplot/sosplot.directive.js
+++ client/app/sosplot/sosplot.directive.js
@@ -1,25 +1,29 @@
 'use strict';
 
 angular.module('diptervApp')
-  .directive('sosplot', function () {
-      $scope.onClick = function (points, evt) {
-        console.log(points, evt);
-      };
-      $scope.labels = [0,1,2,3];
-      $scope.series = ['Series A'];
-      $scope.data = [[0,1,2,3]];
-      function initplot(data){
-        $scope.labels = data.x;
-        $scope.series = [data.uom];
-        $scope.data = [data.y];
-      }
-      $http.get("/api/observe").success(function(data){
-        initplot(data);
-      });
+  .directive('sosplot', function ($http) {
+
     return {
       templateUrl: 'app/sosplot/sosplot-quantity.html',
       restrict: 'EA',
       link: function (scope, element, attrs) {
+        scope.onClick = function (points, evt) {
+          console.log(points, evt);
+        };
+        scope.labels = [0,1,2,3];
+        scope.series = ['Series A'];
+        scope.data = [[0,1,2,3]];
+        function initplot(data){
+          scope.labels = data.x;
+          scope.series = [data.uom];
+          scope.data = [data.y];
+        }
+        $http.get("/api/observe").success(function(data){
+          initplot(data);
+        });
+      },
+      scope: {
+        observation: "="
       }
     };
   });
