afc602b13322d481d7fc47c3595acadaab1c338d
László Velinszky <laszlo.velinszky@meltwater.com>
2015-05-01 12:00:06 +0200

    Added ordering to status page, changed format to table.

--- client/app/status/status.controller.js
+++ client/app/status/status.controller.js
@@ -2,7 +2,12 @@
 
 angular.module('diptervApp')
   .controller('StatusCtrl', function ($scope, $http) {
+    $scope.setOrdering = function (order) {
+      $scope.ordering = order;
+    };
     $http.get("/api/states").success(function(data){
-      $scope.states = data;
+      $scope.states = data.states;
+      $scope.header = data.header;
+      $scope.ordering =  data.header[0].value;
     });
   });
--- server/api/state/state.controller.js
+++ server/api/state/state.controller.js
@@ -5,5 +5,9 @@ var state = require('../../components/status');
 
 // Get list of states
 exports.index = function(req, res) {
-  res.json(200,state.getStates());
+  res.json(200,{"states": state.getStates(),
+    "header": [
+      {"text": "Procedure", "value": "name"},
+      {"text": "Observed Property", "value": "observedPropetyName"}
+    ]});
 };
